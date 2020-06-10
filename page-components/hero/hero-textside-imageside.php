d71ffac9ba8b637d9bb90874ad069d448562a3f5
Max Home <maxwell_kirwin@yahoo.co.uk>
2020-06-10 17:14:04 +0100

    tweaks

--- page-components/hero/hero-textside-imageside.php
+++ page-components/hero/hero-textside-imageside.php
@@ -8,7 +8,7 @@
     
         <?php while ( have_rows('hero_column') ) : the_row(); ?>
 
-            <div class="hero__column">
+           
                 
 
         <?php // FLEXIBLE CONTENT IN COLUMNS ?>
@@ -18,31 +18,35 @@
                         // Title Section
                         if (get_row_layout() == 'hero_title_section') : ?>
 
-                            <div class="hero__text">
-                                <div class="hero__text_inner">
+                            <div class="hero__column">
 
-                                    <?php if (get_sub_field('hero_sub_title')) { ?>
-                                    <h6 class='uppercase font-bold-title sub_title'><span class='hero-line'></span><?php the_sub_field('hero_sub_title'); ?></h6>
-                                    <?php } ?>
-    
-                                    <h1><?php the_sub_field('hero_title'); ?></h1>
-                                    <!-- <div class="underline"></div> -->
-                                
-                                    <?php if (get_sub_field('hero_desc')) { ?>
-                                    <p class='full-width__desc'><?php the_sub_field('hero_desc'); ?></p>
-                                    <?php } ?>
+                                <div class="hero__text">
+                                    <div class="hero__text_inner">
 
-
-                                    <?php 
-                                    $link = get_sub_field('hero_button');
-                                    if( $link ): 
-                                        $link_url = $link['url'];
-                                        $link_title = $link['title'];
-                                        $link_target = $link['target'] ? $link['target'] : '_self';
-                                        ?>
-                                        <a class="button" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
-                                    <?php endif; ?>
+                                        <?php if (get_sub_field('hero_sub_title')) { ?>
+                                        <h6 class='js_textside__sub_title uppercase font-bold-title sub_title'><span class='hero-line'></span><?php the_sub_field('hero_sub_title'); ?></h6>
+                                        <?php } ?>
+        
+                                        <h1 class='js_textside__title'><?php the_sub_field('hero_title'); ?></h1>
+                                        <!-- <div class="underline"></div> -->
+                                    
+                                        <?php if (get_sub_field('hero_desc')) { ?>
+                                        <p class='full-width__desc js_textside__desc'><?php the_sub_field('hero_desc'); ?></p>
+                                        <?php } ?>
+                                       
+                            
+                                        <?php 
+                                        $link = get_sub_field('hero_button');
+                                        if( $link ): 
+                                            $link_url = $link['url'];
+                                            $link_title = $link['title'];
+                                            $link_target = $link['target'] ? $link['target'] : '_self';
+                                            ?>
+                                            <a class="button js_textside__button" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
+                                        <?php endif; ?>
+                                    </div>
                                 </div>
+
                             </div>
                             
                         <?php
@@ -50,14 +54,16 @@
                         //  Image Section
                         elseif (get_row_layout() == 'hero_image_section') : ?>
 
-                            <?php echo wp_get_attachment_image( get_sub_field('hero_image'), 'full'); ?>
-                    
+                        <div class="hero__column js_hero__imageside">
+
+                            <?php echo wp_get_attachment_image( get_sub_field('hero_image'), 'full', '', array('class'=>'js_hero__imageside_image')); ?>
+                        </div>
 
                         <?php endif;
         
                     endwhile; endif; ?>
 
-            </div>
+            
      
 
         <?php  endwhile;  endif; ?>
